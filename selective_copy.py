b0fc093d44fec8514118d9ccb79f129d04193551
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-03-15 18:45:20 -0500

    Added TODO list.

---
+++ selective_copy.py
@@ -0,0 +1,14 @@
+#! python3
+# selective_copy.py — An exercise in organizing files.
+# For more information, see enclosed project_details.txt file.
+
+import os
+import re
+import shutil
+
+
+# TODO: Walk through folder tree.
+
+# TODO: Search for files with certain file extensions.
+
+# TODO: Copy files from current location to a new folder.
