6820fec71a9fdfde78c4558b8283d44c530c48d1
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-03-17 11:53:07 -0500

    Changed a references to folder to directory.

--- selective_copy.py
+++ selective_copy.py
@@ -11,28 +11,28 @@ file_list = []
 
 def selective_copy(file_path):
     """Walk through folder tree, search for files with user-defined file extension,
-    then copy those files to a user-defined folder."""
+    then copy those files to a user-defined directory."""
 
     ext_regex = re.compile(r"(.*)\." + (input("Type desired file extension here: ")))
-    relocation_folder = input("Please type path to destination existing folder: ")
+    copy_dir = input("Please type path to destination existing directory: ")
 
-    for folder_name, subfolders, filenames in os.walk(file_path):
+    for dir_name, subfolders, filenames in os.walk(file_path):
         for filename in filenames:
-            absolute_path = f"{folder_name}/{filename}"
+            absolute_path = f"{dir_name}/{filename}"
             match_object = ext_regex.search(absolute_path)
             if match_object is not None:
                 file_list.append(match_object.group())
 
     length = len(file_list)
-    print(f"Found {length} matches.")
+    print(f"Found {length} matches: {file_list}.")
 
     try:
         for match in range(length):
-            print(f"Copying {file_list[match]} to {relocation_folder}.")
-            shutil.copy(file_list[match], relocation_folder)
+            print(f"Copying {file_list[match]} to {copy_dir}.")
+            shutil.copy(file_list[match], copy_dir)
     except FileNotFoundError:
-        print("Unable to transfer files; folder not found.")
+        print("Unable to transfer files; directory not found.")
 
 
-folder_path = input("Type path to folder you wish to search here: ")
-selective_copy(folder_path)
+dir_path = input("Type path to directory you wish to search here: ")
+selective_copy(dir_path)
