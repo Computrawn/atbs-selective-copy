45eb93c6e66ceed822e5cbcd2ec7f1afb462427b
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-03-17 10:24:52 -0500

    Added try/except clause to handle FNF errors. Trimmed code for clarity.

--- selective_copy.py
+++ selective_copy.py
@@ -14,12 +14,11 @@ def selective_copy(file_path):
     then copy those files to a user-defined folder."""
 
     ext_regex = re.compile(r"(.*)\." + (input("Type desired file extension here: ")))
-    new_folder = input(f"Please type path to destination folder: ")
-    relocation_folder = f"{os.getcwd()}/{new_folder}"
+    relocation_folder = input("Please type path to destination existing folder: ")
 
     for folder_name, subfolders, filenames in os.walk(file_path):
         for filename in filenames:
-            absolute_path = f"{os.getcwd()}/{folder_name}/{filename}"
+            absolute_path = f"{folder_name}/{filename}"
             mo = ext_regex.search(absolute_path)
             if mo is not None:
                 file_list.append(mo.group())
@@ -27,9 +26,12 @@ def selective_copy(file_path):
     length = len(file_list)
     print(f"Found {length} matches.")
 
-    for match in range(length):
-        print(f"Moving {file_list[match]} to {relocation_folder}.")
-        shutil.move(file_list[match], relocation_folder)
+    try:
+        for match in range(length):
+            print(f"Moving {file_list[match]} to {relocation_folder}.")
+            shutil.move(file_list[match], relocation_folder)
+    except FileNotFoundError:
+        print("Unable to transfer files; folder not found.")
 
 
 folder_path = input("Type path to folder you wish to search here: ")
